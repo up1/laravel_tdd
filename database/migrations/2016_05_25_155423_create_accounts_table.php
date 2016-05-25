f6c2ac986b9f7ad59f513577b6413f279591366e
somkiat <somkiat.p@gmail.com>
2016-05-25 23:02:35 +0700

    Add column account_no

--- database/migrations/2016_05_25_155423_create_accounts_table.php
+++ database/migrations/2016_05_25_155423_create_accounts_table.php
@@ -14,6 +14,7 @@ class CreateAccountsTable extends Migration
     {
         Schema::create('accounts', function (Blueprint $table) {
             $table->increments('id');
+            $table->string('account_no');
             $table->timestamps();
         });
     }
