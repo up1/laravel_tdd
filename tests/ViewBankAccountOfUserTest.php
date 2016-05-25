4614faeb458d531d54233967c303f619d607f7ba
somkiat <somkiat.p@gmail.com>
2016-05-25 20:18:55 +0700

    Fixed error by import User in test

--- tests/ViewBankAccountOfUserTest.php
+++ tests/ViewBankAccountOfUserTest.php
@@ -4,6 +4,8 @@ use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 
+use App\User;
+
 class ViewBankAccountOfUserTest extends TestCase{
 
     public function testCreateNewBankAccountByUser(){
