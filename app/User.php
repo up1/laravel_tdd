e1e8eb12b98975d8ae3a2d5c35d01f5536d5b605
somkiat <somkiat.p@gmail.com>
2016-05-25 22:54:20 +0700

    Create function accounts()

--- app/User.php
+++ app/User.php
@@ -23,4 +23,9 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+
+    function accounts() {
+      return $this->hasMany(Account::class);
+    }
 }
