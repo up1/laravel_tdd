59cbda4da48609647bb3c125f8ab9344fa540b82
somkiat <somkiat.p@gmail.com>
2016-05-25 23:03:34 +0700

    Add column user_id to refer back to User

--- database/migrations/2016_05_25_155423_create_accounts_table.php
+++ database/migrations/2016_05_25_155423_create_accounts_table.php
@@ -14,6 +14,7 @@ class CreateAccountsTable extends Migration
     {
         Schema::create('accounts', function (Blueprint $table) {
             $table->increments('id');
+            $table->integer('user_id');
             $table->string('account_no');
             $table->timestamps();
         });
